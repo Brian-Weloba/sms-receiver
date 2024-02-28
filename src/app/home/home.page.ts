c2f6b50dd0a5fec1a66027546e1dd1bbf7f66680
BrianWeloba <brian.n@modefin.com>
2024-02-28 10:13:58 +0300

    Test message

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+declare var SMSReceive: any;
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,44 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  message: string = '';
+  constructor(
+    private toastCtrl: ToastController
+  ) { }
 
-  constructor() {}
+  async presentToast(message: any, position: any, duration: any) {
+    const toast = await this.toastCtrl.create({
+      message: message,
+      position: position,
+      duration: duration
+    });
+    toast.present();
+  }
 
+  next() {
+    this.start();
+  }
+
+  start() {
+    SMSReceive.startWatch(
+      () => {
+        document.addEventListener('onSMSArrive', (e: any) => {
+          var incomingSms = e.data;
+          this.processSMS(incomingSms);
+        })
+      },
+      () => { console.log('start railed') });
+  }
+
+  stop() {
+    SMSReceive.stopWatch(
+      () => { console.log('watch stopped') },
+      () => { console.log('watch stop failed') }
+    )
+  }
+
+  processSMS(data: { body: string | any[]; }) {
+    const message = data.body;
+    this.presentToast(message, 'top', 1500);
+  }
 }
